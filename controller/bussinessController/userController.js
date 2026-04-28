63112e3e2c89283878cbfb55404f32eac63822c9
shasi kumar <shasikumar@varnarc.com>
2026-04-28 17:03:14 +0530

    New Updates on Topcontributors and Suggestions

--- controller/bussinessController/userController.js
+++ controller/bussinessController/userController.js
@@ -1,4 +1,5 @@
 import User from "../../Model/user.js";
+import postModel from  "../../Model/post.js";
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -13,3 +14,63 @@ export const getAllUsers = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+export const getTopContributors = async (req, res) => {
+  try {
+    const topUsers = await postModel.aggregate([
+      {
+        $group: {
+          _id: "$userId",
+          totalPosts: { $sum: 1 }
+        }
+      },
+      {
+        $sort: { totalPosts: -1 }
+      },
+      {
+        $limit: 5
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "user"
+        }
+      },
+      {
+        $unwind: "$user"
+      },
+      {
+        $project: {
+          _id: "$user._id",
+          name: "$user.name",
+          role: "$user.role",
+          totalPosts: 1
+        }
+      }
+    ]);
+
+    res.json(topUsers);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const myId = req.userId;
+    const following = await followModel.find({ followerId: myId });
+
+    const followingIds = following.map(f => f.followedId);
+
+    const suggestions = await User.find({
+      _id: { $nin: [...followingIds, myId] }
+    }).limit(5);
+
+    res.json(suggestions);
+
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
--- router/profileRouter.js
+++ router/profileRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 const router = express.Router();
 import { getProfile, updateProfile, deleteProfile } from "../controller/bussinessController/profileController.js";
-import { getAllUsers } from "../controller/bussinessController/userController.js";
+import { getAllUsers, getTopContributors, getSuggestedUsers } from "../controller/bussinessController/userController.js";
 
 router.get("/profile", getProfile);
 router.put("/profile/:id", updateProfile);
 router.delete("/profile/:id", deleteProfile);
 router.get("/getAllUsers", getAllUsers);
+router.get("/getTopContributers", getTopContributors);
+router.get("/getSuggestions", getSuggestedUsers);
 
 export default router;
